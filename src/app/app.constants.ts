dba9ac6b3580eb90a06bd5a7a179ccccf2f362ef
dusanmila <65555511+dusanmila@users.noreply.github.com>
2021-08-23 23:25:00 +0200

    final comit front

---
+++ src/app/app.constants.ts
@@ -0,0 +1,11 @@
+/*
+export const SMER_URL = 'http://localhost:8083/smer';
+export const PROJEKAT_URL = 'http://localhost:8083/projekat';
+export const GRUPA_URL = 'http://localhost:8083/grupa';
+export const STUDENT_URL = 'http://localhost:8083/student';
+*/
+
+export const SMER_URL = 'https://backend-rva2021-it1-2018.herokuapp.com/smer';
+export const PROJEKAT_URL = 'https://backend-rva2021-it1-2018.herokuapp.com/projekat';
+export const GRUPA_URL = 'https://backend-rva2021-it1-2018.herokuapp.com/grupa';
+export const STUDENT_URL = 'https://backend-rva2021-it1-2018.herokuapp.com/student';
